2596460a75239c4efbd0f28451c2f57f6a744990
mehreenshahzad225 <160320672+mehreenshahzad225@users.noreply.github.com>
2024-04-21 22:31:48 +0500

    sixth commit

---
+++ my_chalk_project/app.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+console.log(chalk.red("hello world"));
+console.log(chalk.green.bold("Typescript best course"));
+console.log(chalk.blue.bold.underline("Typescript is a superset of javascript"));
+console.log(chalk.bgRed("I am very happy 😴"));
---
+++ my_chalk_project/app.ts
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+import chalk from "chalk";
+
+console.log(chalk.red("hello world"));
+console.log(chalk.green.bold("Typescript best course"));
+
+console.log(chalk.blue.bold.underline("Typescript is a superset of javascript"));
+console.log(chalk.bgRed("I am very happy 😴"));
